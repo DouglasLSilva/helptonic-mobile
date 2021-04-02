f17fd17cb049eab8f765e6c706d8d38af865d3af
Lacivitaa <igor.lacivita@hotmail.com>
2021-04-02 19:08:07 -0300

    Update Dio (Login and Register)

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,27 +1,32 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_ftt/interface/api_sheet_repository_interface.dart';
-import 'package:flutter_ftt/model/api_responde.dart';
-import 'package:flutter_ftt/model/client.dart';
+import 'package:flutter_ftt/interface/login_repository_interface.dart';
+import 'package:flutter_ftt/model/login.dart';
+import 'package:flutter_ftt/model/user.dart';
 import 'package:flutter_ftt/repository/api_const.dart';
+import 'package:flutter_ftt/repository/user_const.dart';
 
-class UserRepository implements IApiSheetInterface{
+class UserRepository implements IApiSheetInterfaceLogin {
   Dio dio = Dio();
-  bool isLoading;
+  static String uri = "/login";
 
   @override
-  Future<ApiResponse<User>> fetchData() async {
+  Future<User> loginUser(Login userData) async {
     try {
-      isLoading = true;
-      Response response = await dio.get(Api.apiUrl);
+      Response response = await dio.post(Api.apiUrl + uri,
+          data: {'email': userData.email, 'password': userData.senha});
       var responseData = User.fromJson(response.data);
-      isLoading = false;
-      return ApiResponse(
-        message: response.statusMessage,
-        status: response.statusCode,
-        data: responseData);
-    } catch (e){
-      isLoading = false;
-      return ApiResponse(message: e, status: 500);
+
+      if (response.statusCode == 200) {
+        UserConst.id = responseData.id;
+        UserConst.email = responseData.email;
+        UserConst.name = responseData.name;
+        UserConst.typeBlindess = responseData.typeColorBlindess;
+        UserConst.token = responseData.token;
+      }
+
+      return responseData;
+    } on DioError catch (e) {
+      return User.fromJson(e.response.data);
     }
   }
 }
