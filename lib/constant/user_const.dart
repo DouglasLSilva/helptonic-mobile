23637c96bc584bd9b2ec048199ee921f089ded3d
Lacivitaa <igor.lacivita@hotmail.com>
2021-04-04 12:19:27 -0300

    Visibility sidebar and imageCarousel

---
+++ lib/constant/user_const.dart
@@ -0,0 +1,9 @@
+class UserConst{
+  static String token = '';
+  static String id = '';
+  static String email = '';
+  static String name = '';
+  static String typeBlindess = '';
+  static bool disconnected = true;
+  static List<String> imagesUrl;
+}
